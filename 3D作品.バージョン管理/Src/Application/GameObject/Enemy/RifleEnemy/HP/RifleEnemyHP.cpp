ca9f271dfcb10e699f9aa1463d7f4ead9dfe739b
Ryota Kashiwaya <kd1342269@st.kobedenshi.ac.jp>
2024-10-31 11:22:33 +0900

    敵HPゲージ作成中

---
+++ 3D作品.バージョン管理/Src/Application/GameObject/Enemy/RifleEnemy/HP/RifleEnemyHP.cpp
@@ -0,0 +1,24 @@
+﻿#include "RifleEnemyHP.h"
+#include"../../RifleEnemy/RifleEnemy.h"
+void RifleEnemyHP::Update()
+{
+	std::shared_ptr<RifleEnemy> m_spEnemy = m_wpPearent.lock();
+
+}
+
+void RifleEnemyHP::DrawLit()
+{
+}
+
+void RifleEnemyHP::Init()
+{
+	if (!m_spHpFlamePoly)
+	{
+		m_spHpFlamePoly = std::make_shared<KdSquarePolygon>();
+	}
+
+	if (!m_spHpGagePoly)
+	{
+		m_spHpGagePoly = std::make_shared<KdSquarePolygon>();
+	}
+}
---
+++ 3D作品.バージョン管理/Src/Application/GameObject/Enemy/RifleEnemy/HP/RifleEnemyHP.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class RifleEnemy;
+
+class RifleEnemyHP :public KdGameObject
+{
+public:
+
+	void Update()override;
+
+	void DrawLit()override;
+
+	void Init()override;
+
+	void SetPearent(std::shared_ptr<RifleEnemy> _ptr)
+	{
+		m_wpPearent = _ptr;
+	}
+
+private:
+
+	std::weak_ptr<RifleEnemy> m_wpPearent;
+
+	std::shared_ptr<KdSquarePolygon> m_spHpGagePoly = nullptr;
+	std::shared_ptr<KdSquarePolygon> m_spHpFlamePoly = nullptr;
+
+	Math::Vector3					 m_HPpos;
+
+	Math::Rectangle					 m_rect;
+
+};
